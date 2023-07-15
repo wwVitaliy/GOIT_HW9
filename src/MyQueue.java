f56738871c97c245ef1c315c986b5eba1708e876
wwVitaliy <wwvitaliy@gmail.com>
2023-07-15 22:22:15 +0300

    Task_4

--- src/MyQueue.java
+++ src/MyQueue.java
@@ -1,20 +1,5 @@
 import java.util.Queue;
 
-class MyQueueTest{
-    public static void main(String[] args) {
-        MyQueue<Integer> myQueue = new MyQueue<>();
-        myQueue.add(1);
-        myQueue.add(2);
-        myQueue.add(3);
-        myQueue.add(4);
-        System.out.println("myQueue.peek() = " + myQueue.peek());
-        System.out.println("myQueue.poll() = " + myQueue.poll());
-        System.out.println("myQueue.poll() = " + myQueue.poll());
-        myQueue.clear();
-        System.out.println("myQueue.peek() = " + myQueue.peek());
-    }
-
-}
 
 class MyQueue<T> {
     private int size = 0;
---
+++ src/MyStack.java
@@ -0,0 +1,111 @@
+class MyStackTest {
+    public static void main(String[] args) {
+        MyStack<Integer> myStack = new MyStack<>();
+        myStack.push(1);
+        myStack.push(2);
+        myStack.push(3);
+        System.out.println("myStack.peek() = " + myStack.peek());
+        System.out.println("myStack.pop() = " + myStack.pop());
+        System.out.println("myStack.peek() = " + myStack.peek());
+        System.out.println("myStack.pop() = " + myStack.pop());
+        System.out.println("myStack.peek() = " + myStack.peek());
+        System.out.println("myStack.pop() = " + myStack.pop());
+        System.out.println("myStack.peek() = " + myStack.peek());
+        System.out.println("myStack.pop() = " + myStack.pop());
+        System.out.println("myStack.peek() = " + myStack.peek());
+        System.out.println("----------");
+        myStack.push(3);
+        myStack.push(2);
+        myStack.push(1);
+        System.out.println("myStack.peek() = " + myStack.peek());
+        System.out.println("myStack.pop() = " + myStack.pop());
+        System.out.println("myStack.peek() = " + myStack.peek());
+        System.out.println("myStack.pop() = " + myStack.pop());
+        System.out.println("myStack.peek() = " + myStack.peek());
+        System.out.println("myStack.pop() = " + myStack.pop());
+        System.out.println("myStack.peek() = " + myStack.peek());
+        System.out.println("myStack.pop() = " + myStack.pop());
+        System.out.println("myStack.peek() = " + myStack.peek());
+    }
+}
+
+
+class MyStack<T> {
+    private int size = 0;
+    private Node<T> firstNode = null;
+    private Node<T> lastNode = null;
+
+    public void push(Object value) {
+        if (firstNode == null) {
+            firstNode = new Node<T>((T) value, null, null);
+        } else if (lastNode == null) {
+            lastNode = new Node<T>((T) value, firstNode, null);
+            firstNode.setNextNode(lastNode);
+        } else {
+            lastNode.setNextNode(new Node<>((T) value, lastNode, null));
+            lastNode = lastNode.getNextNode();
+        }
+        size++;
+    }
+
+    public void remove(int index) {
+        if (index > size || index < 1) {
+            return;
+        }
+        if (index == 1) {
+            firstNode = firstNode.getNextNode();
+            firstNode.setPrevNode(null);
+        } else if (index == size) {
+            lastNode = lastNode.getPrevNode();
+            lastNode.setNextNode(null);
+        } else {
+            Node<T> nodeToRemove = firstNode.getNextNode();
+            for (int i = 2; i < index; i++) {
+                nodeToRemove = nodeToRemove.getNextNode();
+            }
+            nodeToRemove.getPrevNode().setNextNode(nodeToRemove.getNextNode());
+            nodeToRemove.getNextNode().setPrevNode(nodeToRemove.getPrevNode());
+            size--;
+        }
+
+
+    }
+
+    public void clear() {
+        firstNode = null;
+        lastNode = null;
+        size = 0;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public T peek() {
+        if (size == 0) {
+            return null;
+        } else if (size == 1) {
+            return firstNode.getValue();
+        } else {
+            return lastNode.getValue();
+        }
+    }
+
+    public T pop() {
+        T result = null;
+        if (size == 0) {
+            return null;
+        } else if (size == 1) {
+            result = firstNode.getValue();
+            firstNode = null;
+        } else {
+            result = lastNode.getValue();
+            lastNode.getPrevNode().setNextNode(null);
+            lastNode = lastNode.getPrevNode();
+        }
+        size--;
+        return result;
+    }
+
+
+}
